5dd6344aec02d6fa46290e8c2f5a391e2823ae1c
Capsius <jan-henrik.capsius@brueder-schlau.de>
2023-03-24 07:49:29 +0100

    .vscode ordner rausgeschmissen

--- Pruefziffern/Program.cs
+++ Pruefziffern/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿namespace Pruefziffern
+{
+    public class Program
+    {
+        static void Main(string[] args){
+        }
+    }
+}
