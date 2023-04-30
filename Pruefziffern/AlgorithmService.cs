f57f8119458b44a317aa11c4ea7d1c33b6419b0e
kurisox <fussballmann85@gmail.com>
2023-04-30 17:40:39 +0200

    driverid funktion added

--- Pruefziffern/AlgorithmService.cs
+++ Pruefziffern/AlgorithmService.cs
@@ -2,51 +2,119 @@ namespace Pruefziffern
 {
     public class AlgorithmService
     {
-        private InputServivce inputServivce;
+        private IInputService inputService;
+        private EANInput eanInput;
+        private DriverIDInput driverIDInput;
+
+        private IAlgorithm algorithm;
         private EANAlgorithm eanAlgorithm;
+        private DriverIDAlgorithm driverIDAlgorithm;
         private int controlNumber;
         private List<int> inputNumbers;
 
         public AlgorithmService()
         {
-            this.inputServivce = new InputServivce();
+            this.eanInput = new EANInput();
+            this.driverIDInput = new DriverIDInput();
             this.eanAlgorithm = new EANAlgorithm();
+            this.driverIDAlgorithm = new DriverIDAlgorithm();
             this.inputNumbers = new List<int>();
         }
 
         public void run()
         {
             bool newInput = true;
-            calculateNumbers();
-            do
-            {
-                switch (System.Console.ReadLine().ToLower())
+            if(selectAlogorithm() == "1"){
+                this.inputService = this.eanInput;
+                this.algorithm = eanAlgorithm;
+                do
                 {
-                    case "j":
-                        calculateNumbers();
+                    calculateNumbers();
+                    System.Console.WriteLine("Prüfzahl: " + this.controlNumber);
+                    System.Console.Write("Vollständige EAN: ");
+                    foreach (int number in this.inputNumbers)
+                    {
+                        System.Console.Write(number);
+                    }
+                    System.Console.Write(this.controlNumber + "\n");
+                    System.Console.WriteLine("Neue Prüfziffer eingeben? [j]a [n]ein");
+                    switch (System.Console.ReadLine().ToLower())
+                    {
+                        case "j":
+                            calculateNumbers();
+                            break;
+                        case "n":
+                            System.Console.WriteLine("Programm wird beendet.");
+                            newInput = false;
+                            break;
+                        default:
+                            System.Console.WriteLine("Ungültige eingabe");
+                            break;
+                    }
+                } while (newInput);
+            }else{
+                this.inputService = this.driverIDInput;
+                this.algorithm = this.driverIDAlgorithm;
+                do
+                {
+                    calculateNumbers();
+                    System.Console.WriteLine("Prüfzahl: " + this.controlNumber);
+                    System.Console.Write("Vollständige FührerscheinID: ");
+                    System.Console.WriteLine("Neue Prüfziffer eingeben? [j]a [n]ein");
+                    switch (System.Console.ReadLine().ToLower())
+                    {
+                        case "j":
+                            calculateNumbers();
+                            break;
+                        case "n":
+                            System.Console.WriteLine("Programm wird beendet.");
+                            newInput = false;
+                            break;
+                        default:
+                            System.Console.WriteLine("Ungültige eingabe");
+                            break;
+                    }
+                } while (newInput);
+            }
+        }
+
+        private String selectAlogorithm(){
+            bool validInput = false;
+            String input = "";
+            System.Console.WriteLine("[1]EAN-Algorithmus\n[2]Führerschein-Algorithmus");
+
+            do{
+                try
+                {
+                    input = System.Console.ReadLine();
+                    switch (input)
+                {
+                    case "1":
+                        validInput = true;
                         break;
-                    case "n":
-                        System.Console.WriteLine("Programm wird beendet.");
-                        newInput = false;
-                        break;
+                    case "2":
+                        validInput = true;
+                        break;    
                     default:
-                        System.Console.WriteLine("Ungültige eingabe");
+                        System.Console.WriteLine("Ungültige Eingabe");
                         break;
                 }
-            } while (newInput);
+                }
+                catch (FormatException e)
+                {
+                    System.Console.WriteLine("Ungültige Eingabe\n");
+                }
+
+            }while(!validInput);
+
+            return input;
+
         }
 
         private void calculateNumbers(){
-            this.inputNumbers = this.inputServivce.dataInput();
-            this.controlNumber = this.eanAlgorithm.calculateControlNumber(this.inputNumbers);
-            System.Console.WriteLine("Prüfzahl: " + this.controlNumber);
-            System.Console.Write("Vollständige EAN: ");
-            foreach (int number in this.inputNumbers)
-            {
-                System.Console.Write(number);
-            }
-            System.Console.Write(this.controlNumber + "\n");
-            System.Console.WriteLine("Neue Prüfziffer eingeben? [j]a [n]ein");
+            this.inputNumbers = this.inputService.dataInput();
+            this.controlNumber = this.algorithm.calculateControlNumber(this.inputNumbers);
+        
         }
     }
 }
