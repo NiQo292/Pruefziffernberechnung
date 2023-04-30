4eda2c7264fe67d2f8e36201b236d6ac412b8df7
kurisox <fussballmann85@gmail.com>
2023-04-30 16:29:09 +0200

    Interface implemented

---
+++ Pruefziffern/EANInput.cs
@@ -0,0 +1,42 @@
+namespace Pruefziffern
+{
+    public class EANInput : IInputService
+    {
+        public List<int> dataInput()
+        {
+            List<int> eanCode = new List<int>();
+            bool validInput = false;
+            do
+            {
+                try
+                {
+                    System.Console.WriteLine("Bitte Prüfziffer eintragen:");
+                    String eanString = Console.ReadLine();
+                    if (eanString.Length < 12)
+                    {
+                        System.Console.WriteLine("Zu wenig Ziffern eingegeben\n");
+                    }
+                    else if (eanString.Length > 12)
+                    {
+                        System.Console.WriteLine("Zu viele Ziffern eingegeben\n");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < eanString.Length; i++)
+                        {
+                            eanCode.Add(Convert.ToInt32(new String(eanString[i], 1)));
+                        }
+                        validInput = true;
+                        System.Console.WriteLine("Prüfziffer gültig.\n");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    System.Console.WriteLine("Ungültige Eingabe\n");
+                    eanCode.Clear();
+                }
+            } while (!validInput);
+            return eanCode;
+        }
+    }
+}
