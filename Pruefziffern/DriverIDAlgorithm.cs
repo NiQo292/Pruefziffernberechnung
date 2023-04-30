ba0de5747ea65fc2c18dd24551223a6cc1669668
kurisox <fussballmann85@gmail.com>
2023-04-30 17:40:50 +0200

    algorithm implemented

--- Pruefziffern/DriverIDAlgorithm.cs
+++ Pruefziffern/DriverIDAlgorithm.cs
@@ -1,7 +1,18 @@
 namespace Pruefziffern
 {
-    public class DriverIDAlgorithm
+    public class DriverIDAlgorithm : IAlgorithm
     {
-        
+        public int calculateControlNumber(List<int> inputNumbers)
+        {
+            int controlNumber = 0;
+            int subtotal = 0;
+            int multiplier = 9;
+            for (int i = 0; i < inputNumbers.Count; i++){
+                subtotal += inputNumbers[i] * multiplier;
+                multiplier -= 1;
+            }
+            controlNumber = subtotal % 11;
+            return controlNumber;
+        }
     }
 }
